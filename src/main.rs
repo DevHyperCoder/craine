dcaab08564181ad622459a4bfcf7f91a8b7d3b77
DevHyperCoder <arduinoleo88@gmail.com>
2021-04-16 11:47:17 +0530

    ✨ Recursive traversal through dom_tree

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,49 @@
-use html_parser::{Dom, Result};
+use html_parser::{Dom, Node::*, Result};
 
 fn main() -> Result<()> {
-    let html = "<p> lorem ipsum </p>";
-    let json = Dom::parse(html);
-    println!("{:#?}", json.unwrap());
+    let html = r#"
+    <p> lorem ipsum </p>
+    <br/>
+    <form action="asdf" method="get">
+    <input type="number">
+    </form>
+    <FancyHR />
+    "#;
+
+    let dom_tree = Dom::parse(html);
+
+    extract(dom_tree.unwrap().children);
+
     Ok(())
+}
+
+// Recursive function to go through the DOM tree and printout a basic structure
+fn extract(dom_tree: Vec<html_parser::Node>) {
+    for i in dom_tree {
+        match i {
+            Element(element) => {
+                // for self closing tags
+                if element.variant == html_parser::ElementVariant::Void {
+                    println!("<{} />", element.name);
+                    continue;
+                }
+
+                println!("<{}>", &element.name);
+
+                // Recursive child extraction
+                if element.children.len() > 0 {
+                    extract(element.children)
+                }
+
+                println!("</{}>", element.name)
+            },
+            Text(text) =>{
+                println!("{}",text);
+            },
+
+            Comment(_) => {
+                println!("comment");
+            }
+        }
+    }
 }
