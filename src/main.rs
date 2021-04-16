ee7a7f7a6e8fda8309d833f930195113a07dcfc1
DevHyperCoder <arduinoleo88@gmail.com>
2021-04-16 19:20:25 +0530

    ✨ Add handling for attributes classes and id

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use html_parser::{Dom, Node::*, Result};
+use html_parser::{Dom, Node::*};
 
-fn main() -> Result<()> {
+fn main() {
     let html = r#"
-    <p> lorem ipsum </p>
+    <p class="t-2 w-100"> lorem ipsum </p>
     <br/>
-    <form action="asdf" method="get">
+    <form id="login-form" action="asdf" method="get">
     <input type="number">
     </form>
     <FancyHR />
@@ -13,8 +13,6 @@ fn main() -> Result<()> {
     let dom_tree = Dom::parse(html);
 
     extract(dom_tree.unwrap().children);
-
-    Ok(())
 }
 
 // Recursive function to go through the DOM tree and printout a basic structure
@@ -22,13 +20,37 @@ fn extract(dom_tree: Vec<html_parser::Node>) {
     for i in dom_tree {
         match i {
             Element(element) => {
+                println!("<{} ", element.name);
+
+                // add classes
+                if element.classes.len() > 0 {
+                    print!("class=\"");
+                    for i in element.classes {
+                        print!("{} ", i);
+                    }
+                    print!("\"");
+                }
+
+                // add id
+                match element.id {
+                    None => {},
+                    Some(id) => {
+                        println!("id=\"{}\"", id);
+                    }
+                }
+
+                // TODO add support for non-value stuff as well
+                for i in element.attributes {
+                    println!("{}=\"{:?}\"", i.0, i.1);
+                }
+
                 // for self closing tags
                 if element.variant == html_parser::ElementVariant::Void {
-                    println!("<{} />", element.name);
+                    println!("/>");
                     continue;
                 }
 
-                println!("<{}>", &element.name);
+                println!(">");
 
                 // Recursive child extraction
                 if element.children.len() > 0 {
@@ -36,10 +58,10 @@ fn extract(dom_tree: Vec<html_parser::Node>) {
                 }
 
                 println!("</{}>", element.name)
-            },
-            Text(text) =>{
-                println!("{}",text);
-            },
+            }
+            Text(text) => {
+                println!("{}", text);
+            }
 
             Comment(_) => {
                 println!("comment");
