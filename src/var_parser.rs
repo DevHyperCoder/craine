a5b7d71f6f96e0d638da4f861705eb76fbeea2a1
DevHyperCoder <arduinoleo88@gmail.com>
2021-05-05 14:47:54 +0530

    💡 Add documentation and syntax

--- src/var_parser.rs
+++ src/var_parser.rs
@@ -4,13 +4,16 @@ use regex::Regex;
 use std::collections::HashMap;
 
 //TODO multiline support later
-//TODO remove the mut cuz its not put anyways
-//TODO add better docs
 /**
- * get var using regex
+ * Gets all the variable definitions present in the content.
+ *
+ * Syntax:
+ *  {var_name||var_value}
+ *
+ * Error: 
+ *  - If the variable name is empty
  */
-pub fn get_variables(content: &mut Vec<&str>) -> Result<HashMap<String, String>, ErrorType> {
-    //TODO lazy static later
+pub fn get_variables(content: &Vec<&str>) -> Result<HashMap<String, String>, ErrorType> {
 
     lazy_static! {
         static ref VARIABLE_RE: Regex =
@@ -18,16 +21,13 @@ pub fn get_variables(content: &mut Vec<&str>) -> Result<HashMap<String, String>,
     }
 
     let mut variables = HashMap::new();
-    let mut var_line = vec![];
-    // Two iterations ; TODO refactor
 
-    for (index, i) in content.iter().enumerate() {
+    for i in content {
         match VARIABLE_RE.captures(&i) {
             None => {}
             Some(capture) => {
                 let name = capture.get(1).map_or("", |m| m.as_str());
                 let value = capture.get(2).map_or("", |m| m.as_str());
-                var_line.push(index);
 
                 if name.is_empty() {
                     return Err(ErrorType::Parse("Empty variable name"));
@@ -38,22 +38,28 @@ pub fn get_variables(content: &mut Vec<&str>) -> Result<HashMap<String, String>,
         }
     }
 
-    for i in var_line {
-        content[i] = "";
-    }
 
     Ok(variables)
 }
 
-// TODO docs
 /**
- * replace var using regex
+ * Replaces variables in the content string. 
+ * TODO in the future, should consider to use a regex or somekind of variable not defined check
+ * 
+ * Syntax:
+ *  (var_name)
+ * 
+ * TODO perhaps make it with {var_name} for consistency. (DEBUG later becuase sometimes the
+ * variable is not replaced correectly. Might have to do with format strings. perhaps using
+ * concatenation might help us)
  */
 pub fn replace_variables(
     content: &String,
     variables: HashMap<String, String>,
 ) -> Result<String, ErrorType> {
+
     let mut c = content.to_string();
+
     for key in variables.keys().into_iter() {
         let var_name = format!("({})", key);
         let value = variables.get(key).unwrap();
